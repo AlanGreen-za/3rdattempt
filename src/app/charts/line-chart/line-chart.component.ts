b93fd871cd2f5847ced4076cd7151732cc55766e
AlanGreen-za <amgautosoftware@gmail.com>
2021-02-03 19:25:18 +0200

    added the painful linebar

--- src/app/charts/line-chart/line-chart.component.ts
+++ src/app/charts/line-chart/line-chart.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { LINE_CHART_COLORS } from '../../shared/chart.colors';
+
+const LINE_CHART_SAMPLE_DATA: any[] = [
+  {data: [4, 8, 3, 5, 7, 4], label: 'DMS PRO'},
+  {data: [2, 3, 4, 0, 4, 2], label: 'DMS ACCOUNTING'},
+  {data: [3, 1, 1, 2, 3, 5], label: 'WEBMASTER'},
+];
+
+const LINE_CHART_LABELS: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'];
+
+
 
 @Component({
   selector: 'app-line-chart',
@@ -9,7 +20,21 @@ export class LineChartComponent implements OnInit {
 
   constructor() { }
 
+  lineChartData = LINE_CHART_SAMPLE_DATA;
+  lineChartLabels = LINE_CHART_LABELS;
+  lineChartOptions: any = {
+    responsive: true
+  };
+
+  lineChartLegend: true;
+  lineChartType = 'line';
+  lineChartColors = LINE_CHART_COLORS;
+
+
+
   ngOnInit(): void {
   }
+
+  
 
 }
